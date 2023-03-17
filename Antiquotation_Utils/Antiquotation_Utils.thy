50ee89e666243a1c127e4168bdf97e3eab426321
Kevin Kappelmann <accounts@kappelmann.me>
2023-03-17 11:17:41 +0100

    feat(*) upgrade to hierarchical logging + better antiquotations

---
+++ Antiquotation_Utils/Antiquotation_Utils.thy
@@ -0,0 +1,13 @@
+\<^marker>\<open>creator "Kevin Kappelmann"\<close>
+section \<open>Antiquotation Utils\<close>
+theory Antiquotation_Utils
+  imports Pure
+begin
+
+paragraph \<open>Summary\<close>
+text \<open>Utilities for ML antiquotations.\<close>
+
+ML_file\<open>antiquotation_util.ML\<close>
+
+end
+
--- ROOT
+++ ROOT
@@ -1,6 +1,12 @@
 chapter "Logging"
 
 session Logging = "Pure" +
+  directories
+    Antiquotation_Utils
+    Data_Structures
+    ML_Attributes
   theories
     Logging
+    ML_Attributes
+    Setup_Result_Commands
 
